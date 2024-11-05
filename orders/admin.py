3559917e4be48bf396f4c3117bbadccd331499f9
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-05 12:42:12 -0300

    feat: creating model and script test

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'client',
+        'product',
+        'amount',
+        'total_price',
+        'status'
+    ]
+    ordering = ['-id']
+    search_fields = ['id', 'client', 'product']
+    list_editable = ['amount', 'total_price']
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = ['id', 'client', 'product']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    STATUS_CHOICES = [
+        ('WAI', 'Aguardando pagamento'),
+        ('PRO', 'Processando'),
+        ('SHI', 'Enviado'),
+        ('DEL', 'Entregue'),
+        ('CAN', 'Cancelado'),
+    ]
+
+    client = models.CharField(max_length=100) # possivel uso de foreign key aqui Clients
+    product = models.CharField(max_length=100) # possivel uso de foreign key aqui Products
+    amount = models.BigIntegerField(default=0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='WAI')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Encomenda {self.id} - {self.client}, {self.status}'
