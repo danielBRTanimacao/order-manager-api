33ab9566c87430507e73f245b8b1d91b34b7a9e5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-06 12:03:20 -0300

    created filter

--- orders/api.py
+++ orders/api.py
@@ -1,10 +1,12 @@
 from ninja import Router
 from .schemas import OrderSchema
 from .models import Order
+from typing import List, Optional
 
 orders_router = Router()
 
-@orders_router.get('/', response={200: list[OrderSchema]})
-def index(request):
+@orders_router.get('/', response={200: List[OrderSchema]})
+def index(request, q: Optional[int] = None):
     orders = Order.objects.all()
+    if q is not None: orders = orders[:q]
     return [OrderSchema.from_orm(order) for order in orders]
