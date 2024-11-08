6dd2b42bc63f42e087d9668f8367f0d3921ab3b3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-08 18:25:28 -0300

    raise error

--- orders/api.py
+++ orders/api.py
@@ -8,5 +8,18 @@ orders_router = Router()
 @orders_router.get('/', response={200: List[OrderSchema]})
 def index(request, q: Optional[int] = None):
     orders = Order.objects.all()
-    if q is not None: orders = orders[:q]
+    if q is not None: 
+        # if q < 0:
+        #     return [OrderSchema] erro aqui
+        orders = orders[:q]
     return [OrderSchema.from_orm(order) for order in orders]
+
+@orders_router.post('/', response={201: List[OrderSchema]})
+def create(request, order: OrderSchema):
+    if request.user.is_authenticated:
+        return order
+    return {
+        "title": "Raise error authentication",
+        "ResponseStatus": 401,
+        "detail": "Erro: Não autorizado usuario não autenticado!"
+    }
