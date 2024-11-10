f1d00a45c97590839f5faeea504d7b7e640e5a02
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-10 15:32:38 -0300

    creating new validation on gets

--- orders/api.py
+++ orders/api.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.forms.models import model_to_dict
+from django.http import Http404
 from ninja import Router
 from .schemas import OrderGetSchema, OrderPostSchema
 from .models import Order
@@ -20,8 +21,11 @@ def list_orders(request, q: Optional[int] = None):
 
 @orders_router.get('/{id}', response={200: dict, 400: dict})
 def specific_order(request, id: int):
-    order = get_object_or_404(Order, id=id)
-    return model_to_dict(order)
+    try:
+        order = get_object_or_404(Order, id=id)
+        return model_to_dict(order)
+    except Http404:
+        return raise_detail_message("Nao encontrado", 404, "Encomenda nao encontrada!")
 
 @orders_router.post('/', response={200: dict})
 def create(request, order_schema: OrderPostSchema):
@@ -45,6 +49,6 @@ def update(request, id: int, order_schema: OrderPostSchema):
     for attr, value in order_schema.dict().items():
         setattr(order_unique, attr, value)
     order_unique.save()
-    
+
     return raise_detail_message("Order update", 200, "Objeto modificado com sucesso")
 
